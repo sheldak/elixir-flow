cf57fcb9803b85a9c61dc1f6e453963b9e8dccc1
Samuel <sheldak426@gmail.com>
2020-06-08 14:12:36 +0200

    initial

---
+++ lib/elixir_flow.ex
@@ -0,0 +1,19 @@
+defmodule ElixirFlow do
+  @moduledoc """
+  Documentation for `ElixirFlow`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirFlow.hello()
+      :world
+
+  """
+  def hello do
+    :world
+#    IO.puts "Hello"
+  end
+end
